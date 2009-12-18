c718964cb0486ddec4ecefef45ee12e4c5e170cf
Rajesh J Advani <rajeshjadvani@yahoo.com>
2009-12-18 18:10:23 +0530

    Added help guide.

--- gadgets/organic-poetry/organic.js
+++ gadgets/organic-poetry/organic.js
@@ -293,6 +293,15 @@ function clearState() {
 	wave.getState().reset();
 }
 
+function toggleGuide() {
+	var guide = $("#user-guide");
+	if (guide.hasClass("hidden")) {
+		guide.removeClass("hidden");
+	} else {
+		guide.addClass("hidden");
+	}
+}
+
 /*
  * This function should be called on first load of the gadget.
  * This should setup initial state of elements if not already set.
@@ -317,6 +326,7 @@ function stateUpdated() {
 		$("#clear").click(clearState);
 		$("#add").click(addWords);
 		$("#delete").click(deleteSelected);
+		$("#user-guide-link").click(toggleGuide);
 	}
 
 	//Update last Id from state.
